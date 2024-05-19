52699cf6b20353a53bc12b402eb2eadccb03b09a
Carlistos10 <Maranhasgc@gmail.com>
2024-05-19 20:12:46 +0200

    Jwt implementado

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
-
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body
@@ -9,6 +9,7 @@ export const register = async (req, res) => {
 
         const passwordHash = await bcrypt.hash(password, 10)
 
+        //creamos el usuario
 
         const newUser = new User({
             username,
@@ -18,7 +19,23 @@ export const register = async (req, res) => {
 
         const userSaved = await newUser.save()
 
+        //creamos un token para el usuario
+        jwt.sign(
+            {
+                id: userSaved._id,
+            },
+            "Token_Secreto",
+            {
+                expiresIn: "1d"
+            },
+            (err, token) => {
+                if (err) console.log(err);
+                res.cookie('token', token)
+            }
+        );
 
+
+        //ofrecemos solo los datos del usuario que va a manejar el front
 
         res.json({
             id: userSaved._id,
@@ -26,10 +43,10 @@ export const register = async (req, res) => {
             email: userSaved.email,
             createdAt: userSaved.createdAt,
             updatedAt: userSaved.updatedAt,
-
         })
+
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ message: error.message });
     }
 
 
